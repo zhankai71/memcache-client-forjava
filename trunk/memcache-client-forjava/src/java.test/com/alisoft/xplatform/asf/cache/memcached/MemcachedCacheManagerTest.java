220556f93abc0272f4f5a16f5ea6185d4b3c7d09
cenwenchu@163.com <cenwenchu@163.com@113c8d86-8945-11dd-a143-0dd59a09f4aa>
2009-07-30 01:26:49 +0000

    cache 2.5.2 version

---
+++ trunk/memcache-client-forjava/src/java.test/com/alisoft/xplatform/asf/cache/memcached/MemcachedCacheManagerTest.java
@@ -0,0 +1,48 @@
+package com.alisoft.xplatform.asf.cache.memcached;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.alisoft.xplatform.asf.cache.ICacheManager;
+import com.alisoft.xplatform.asf.cache.IMemcachedCache;
+
+public class MemcachedCacheManagerTest
+{
+	ICacheManager<IMemcachedCache> manager;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		manager = CacheUtil.getCacheManager(IMemcachedCache.class,
+				MemcachedCacheManager.class.getName());
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testStartAndStop()
+	{
+		try
+		{
+			manager.start();
+			manager.stop();
+			
+			Thread.sleep(1000);
+			
+			
+		}
+		catch(Exception ex)
+		{
+			org.junit.Assert.assertFalse(true);
+			ex.printStackTrace();
+		}
+		
+		
+	}
+
+}
